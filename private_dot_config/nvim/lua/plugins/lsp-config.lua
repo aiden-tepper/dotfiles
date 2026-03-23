4572c5a6928044d734fe35e7f27599366ea585c7
Aiden Tepper <aidenjtep@gmail.com>
2026-03-22 22:34:56 -0400

    nvim changes

--- private_dot_config/nvim/lua/plugins/lsp-config.lua
+++ private_dot_config/nvim/lua/plugins/lsp-config.lua
@@ -1,52 +1,20 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "ts_ls", "bashls", "eslint", "taplo", "ruff" },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			vim.lsp.enable("rust_analyzer")
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({
-			})
-			lspconfig.ts_ls.setup({
-			})
-			lspconfig.bashls.setup({
-			})
-			lspconfig.eslint.setup({
-				on_attach = function(client, bufnr)
-					vim.api.nvim_create_autocmd("BufWritePre", {
-						buffer = bufnr,
-						command = "EslintFixAll",
-					})
-				end,
-			})
-			lspconfig.taplo.setup({
-			})
-			lspconfig.ruff.setup({
-			})
-			lspconfig.clangd.setup({
-				cmd = { "clangd-12" },
-			})
+	"neovim/nvim-lspconfig",
+	lazy = false,
+	config = function()
+		vim.lsp.enable("lua_ls")
+		vim.lsp.enable("ts_ls")
+		vim.lsp.enable("bash_ls")
+		vim.lsp.enable("eslint")
+		vim.lsp.enable("taplo")
+		vim.lsp.enable("ruff")
+		vim.lsp.enable("clangd")
+		vim.lsp.enable("nixd")
+		vim.lsp.enable("rust_analyzer")
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			-- vim.keymap.set({ "n" }, "<leader>a", vim.lsp.buf.code_action, {})
-			-- who needs code actions
-		end,
-	},
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+		-- vim.keymap.set({ "n" }, "<leader>a", vim.lsp.buf.code_action, {})
+		-- who needs code actions
+	end,
 }
