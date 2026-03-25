9fc6eef5d4de37e65a66698766f044f863e41be1
Aiden Tepper <aidenjtep@gmail.com>
2026-03-24 22:16:35 -0400

    lots

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,6 +1,21 @@
 if status is-interactive
     set fish_greeting # disable greeting
     starship init fish | source
+		zoxide init fish | source
+
+		alias cz "chezmoi"
+		alias czcd "chezmoi cd"
+		alias czad "chezmoi add"
+		alias czra "chezmoi re-add"
+		alias czed "chezmoi edit"
+		alias czap "chezmoi apply"
+		alias czd "chezmoi diff"
+
+		alias ls "eza -l"
+		alias ll "eza -lgo"
+		alias la "eza -laa"
+		alias lla "eza -lgoaa"
+		alias lt "eza -T"
 end
 
 set -gx EDITOR nvim
